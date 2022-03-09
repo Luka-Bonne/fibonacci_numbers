e7c4e3424648de7c21fb8687ddfc90a0ac74cae2
Masha Abu Saif <m.i.as006900@gmail.com>
2022-03-10 00:50:26 +0400

    Теперь fib выводит значения последовательно

--- fibb.py
+++ fibb.py
@@ -1,11 +1,12 @@
 def fib(n):
-    if n == 1:
-        return 0
-    elif n == 2:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
+    a, b = 0, 1
+    for i in range(n):
+        print(a)
+        t = b
+        b = a + b
+        a = t
+    return
 
 
 
-print(fib(10))
+fib(10)
